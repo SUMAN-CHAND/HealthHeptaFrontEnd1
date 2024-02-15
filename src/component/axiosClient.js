ca6b2b99dffff151b3c9812c24bf73fe651bc5d9
Suman <sumanchand16403@gmail.com>
2024-02-15 13:53:19 +0530

    Commit on 15/02/2024 for session problem in  aws by suman

--- src/component/axiosClient.js
+++ src/component/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8081'
+    baseURL: 'http://54.91.173.80:8081'
 })
+//54.91.173.80
 
 export default axiosClient;
